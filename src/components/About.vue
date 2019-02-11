270a3fad0ce63737e9708d74d4a97b4db0ed3c79
pennyswh <27347285@qq.com>
2019-02-11 17:14:05 +0800

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="about container">
+        <h1 class="page-header">希望给大家更好的课程！</h1>
+        <p>如果需要，可以加群索取！</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
